a672b88e27d5e10d717dbfdd2014797829f6e0ad
Kseniia dz 2 <botman12011993@gmail.com>
2026-01-25 19:08:47 +0100

    add ty6.py

---
+++ 02/05/ty.py
@@ -0,0 +1,11 @@
+a = number= int(input("Enter first number: "))
+b = number2= int(input("Enter second number: "))
+c = number3= input("Enter operation (+.-,*,/):")
+if c == "+":
+    print (a+b)
+elif c == "-":
+    print (a-b)
+if c == "*":
+    print (a*b)
+if b != 0:
+    print (a/b)
