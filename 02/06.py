abf4f448df09e6aa36752aef77a7028e852ab5e6
Kseniia dz 2 <botman12011993@gmail.com>
2026-01-25 16:27:52 +0100

    add 06.py

---
+++ 02/06.py
@@ -0,0 +1,36 @@
+import  math
+#2.1
+a = 5
+b = a ** 2
+print (b)
+#2.2
+q = 2
+w = 4
+e = 6
+r = (q + w + e ) / 3
+print (r)
+#2.3
+t = 135
+y = t //60
+u = t % 60
+print (y , u)
+#2.4
+i = 1000
+o = i * 15 / 100
+p = i - o
+print (p)
+#2.5
+s = 347
+d = s % 10
+print (d)
+#2.6
+f = 5
+g = 3
+h = 2 * (f + g)
+print (h)
+#2.7
+k ="3459"
+print (k[0])
+print (k[1])
+print (k[2])
+print (k[3])
