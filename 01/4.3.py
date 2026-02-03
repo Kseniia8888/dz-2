32574995d5b1f81345db07e198cad39a6129f67c
Kseniia dz 2 <botman12011993@gmail.com>
2026-02-03 15:01:30 +0100

    dz3,j

---
+++ 01/4.3.py
@@ -0,0 +1,16 @@
+import random
+
+# create a list with random length from 3 to 10
+length = random.randint(3, 10)
+numbers = []
+
+i = 0
+while i < length:
+    numbers.append(random.randint(1, 10))
+    i = i + 1
+
+# create a new list with 3 elements
+new_list = [numbers[0], numbers[2], numbers[-2]]
+
+print(numbers)
+print(new_list)
