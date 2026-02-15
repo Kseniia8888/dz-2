8b13d25cd7a3363ac1178fe976b898dea16b1ada
Kseniia dz 2 <botman12011993@gmail.com>
2026-02-15 14:13:31 +0100

    dzzz 7.4

---
+++ 01/dz 7.4.py
@@ -0,0 +1,10 @@
+def common_elements():
+    list_3 = [x for x in range(100) if x % 3 == 0]
+    list_5 = [x for x in range(100) if x % 5 == 0]
+
+    common = set(list_3) & set(list_5)
+
+    return common
+
+
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
